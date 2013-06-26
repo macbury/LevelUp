017d481e1e2ad74d5a8352090823c1a64304c433
macbury <macbury@gmail.com>
2013-06-26 12:39:54 +0200

    New fancy style

--- src/com/macbury/levelup/activity/LoginActivity.java
+++ src/com/macbury/levelup/activity/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.macbury.levelup.activity;
 
+import com.macbury.levelup.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -7,12 +9,10 @@ public class LoginActivity extends Activity {
 
   public static final String EXTRA_AUTH_TOKEN_TYPE = null;
 
-  /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-  
-      // TODO Auto-generated method stub
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
   }
 
 }
